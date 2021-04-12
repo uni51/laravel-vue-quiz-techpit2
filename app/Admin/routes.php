9e2c00d5bd10a94040522114c360fe51f4433c4a
uni51 <unieye51@gmail.com>
2021-04-12 16:18:42 +0900

    11-4: Laravel-Adminのカスタマイズ -キーワードの管理の実装-

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,4 +16,5 @@ Route::group([
     $router->resource('/categories', CategoryController::class);
     $router->resource('/users', UserController::class);
     $router->resource('/quizzes', QuizController::class);
+    $router->resource('/keywords', KeywordController::class);
 });
